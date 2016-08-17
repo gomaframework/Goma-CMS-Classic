8a723781bda80ef72811a9cc562f0098e68782a5
Daniel Gruber <daniel@ibpg.eu>
2016-08-17 12:17:33 +0200

    Squashed 'system/' changes from 21b0c94..4f67372
    4f67372 - Changed Mode
    f01485c - update merge
    032eb2c - merge shell
    6d9cfd8 - system files
    d51994c - Update
    
    git-subtree-dir: system
    git-subtree-split: 4f673729febf3385a15dac58e8fc1f837f8a4aaf

---
+++ merge.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+BASEDIR=$(dirname $0)
+echo "Script location: ${BASEDIR}"
+cd $BASEDIR
+cd ..
+
+git branch -D backport-system
+git subtree split -P system -b backport-system
+git push framework backport-system:develop
+git branch -D backport-system
+sleep 3
