e0c6ee22dde10383092345dd23901bb1b5bfa5de
Daniel Gruber <daniel@ibpg.eu>
2016-10-10 17:38:12 +0200

    - Group Admin + User Admin Improvements

--- system/core/admin/groupAdmin.php
+++ system/core/admin/groupAdmin.php
@@ -1,38 +1,37 @@
-<?php
+<?php defined("IN_GOMA") OR die();
+
 /**
-  *@package goma framework
-  *@link http://goma-cms.org
-  *@license: LGPL http://www.gnu.org/copyleft/lesser.html see 'license.txt'
-  *@author Goma-Team
-  * last modified: 21.10.2013
-  * $Version 1.0.4
-*/
-
-defined('IN_GOMA') OR die('<!-- restricted access -->'); // silence is golden ;)
-
+ * Group-Admin-Panel
+ *
+ * @package goma framework
+ * @link http://goma-cms.org
+ * @license: LGPL http://www.gnu.org/copyleft/lesser.html see 'license.txt'
+ * @author Goma-Team
+ * @version 1.1
+ */
 class groupAdmin extends adminItem {
 	/**
 	 * text
-	*/
+	 */
 	public $text = '{$_lang_groups}';
-	
+
 	/**
 	 * permissions
-	*/
+	 */
 	public $rights = "canManagePermissions";
-	
+
 	/**
 	 * sort
-	*/
+	 */
 	public $sort = "700";
-	
+
 	/**
 	 * models
-	*/
+	 */
 	public $model = "group";
-	
+
 	static $icon = "system/templates/admin/images/group.png";
-	
+
 	static $less_vars = "tint-brown.less";
 
 	/**
@@ -43,28 +42,28 @@ class groupAdmin extends adminItem {
 	public function historyURL() {
 		return "admin/history/group";
 	}
-	
+
 
 	/**
 	 * logic
-	*/
+	 */
 	public function index() {
-		
+
 		$config = TableFieldConfig_Editable::create();
 		$config->getComponentByType("TableFieldDataColumns")->setDisplayFields(array(
 			"id"		=> "ID",
 			"users.count" => lang("usercount"),
-			"name"		=> lang("name")
+			"Name"		=> lang("name")
 		))->setFieldFormatting(array(
-			"users.count" => '$users.count ' . lang("usercount")
+			"users.count" => '<a href="admin/user/?groupid=$id&redirect='.urlencode(ROOT_PATH  . $this->namespace . URLEND).'">$users.count ' . lang("usercount") . '</a>'
 		));
 		$config->removeComponent($config->getComponentByType("TableFieldToolbarHeader"));
 		$config->getComponentByType("TableFieldPaginator")->perPage = 20;
-		
+
 		$form = new Form($this, "form_groupadmin", array(
 			new TableField("groupTable", lang("groups"), $this->modelInst(), $config)
 		));
-		
+
 		return $form->render();
 	}
 }
