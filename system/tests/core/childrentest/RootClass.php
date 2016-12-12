aedf0d7baf10c927f44a0005b9b7573a73a473dc
Daniel Gruber <daniel@ibpg.eu>
2016-12-13 00:26:33 +0100

    - add test for getChildren with namespaces

---
+++ system/tests/core/childrentest/RootClass.php
@@ -0,0 +1,18 @@
+<?php
+namespace Goma\Test\Children;
+defined("IN_GOMA") OR die();
+/**
+ * Unit-Tests for ClassInfoTests.
+ *
+ * @package		Goma\Test
+ *
+ * @author		Goma-Team
+ * @license		GNU Lesser General Public License, version 3; see "LICENSE.txt"
+ */
+class RootClass {
+
+}
+
+class ChildClass extends RootClass {
+
+}
